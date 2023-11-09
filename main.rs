b34ec12a13bd3191115f4ebd73d70062678eca59
Stephan Sommer <steven.sommer@go.winona.edu>
2023-11-09 17:49:57 -0600

    parse_config added

--- main.rs
+++ main.rs
@@ -1,7 +1,27 @@
 // Contiguous Memory Allocation Rust Program
-
-use strategies::{};
+use std::collections::HashMap;
 
 fn main() {
     println!("Hello World!");
+}
+
+fn parse_config(s: &str) -> HashMap<String, String> {
+    s
+      .lines()
+      .map(|line| line.trim())
+      .map(|pair| pair.split("="))
+      .map(|mut pair| (pair.next().unwrap().into(), pair.next().unwrap().into()))
+      .collect()
+}
+
+#[test]
+fn test_parse_config() {
+    assert_eq!(
+        parse_config(
+            "foo=bar
+            baz=qux
+            qux=foobar"
+        ),
+        [("foo".into(),"bar".into()), ("baz".into(), "qux".into()), ("qux".into(), "foobar".into())].into_iter().collect()
+    )
 }
