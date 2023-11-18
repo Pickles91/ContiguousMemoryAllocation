fd4d5af3ec75d427889c8498627ab31578593ea9
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2023-11-18 14:32:18 -0600

    refactor: add in lib

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,4 @@
-// Contiguous Memory Allocation Rust Program
-use std::collections::HashMap;
-
 fn main() {
     println!("Hello World!");
 }
 
-fn parse_config(s: &str) -> HashMap<String, String> {
-    s
-      .lines()
-      .map(|line| line.trim())
-      .map(|pair| pair.split("="))
-      .map(|mut pair| (pair.next().unwrap().into(), pair.next().unwrap().into()))
-      .collect()
-}
-
-#[test]
-fn test_parse_config() {
-    assert_eq!(
-        parse_config(
-            "foo=bar
-            baz=qux
-            qux=foobar"
-        ),
-        [("foo".into(),"bar".into()), ("baz".into(), "qux".into()), ("qux".into(), "foobar".into())].into_iter().collect()
-    )
-}
